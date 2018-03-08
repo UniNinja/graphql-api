495e67486c773a44c74189252ad0149ad6412956
Harry Collins <harry@Harrys-MacBook-Pro.local>
2018-03-08 15:22:06 +0000

    returning info based on username

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+require('dotenv').config()
+
+
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://kay:UniNinja@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+MongoClient.connect(uri, function(err, db) {
+  console.log('Connection to DB has been made!');
+  db.close();
+});
+
+
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const app = express()
@@ -9,7 +20,16 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }))
 
-app.get('/', (req, res) => res.send("Hello from UniNinja !"))
+//app.get('/', (req, res) => res.send("Hello from UniNinja !"))
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  if (req.query.username == "loic") {
+    res.send('iOS > Android');
+  } else {
+    res.send('Android > iOS');
+  }
+})
 
 // run server on port 3000
 app.listen('3000', _ => console.log('Server is listening on port 3000...'))
