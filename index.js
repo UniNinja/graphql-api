7da7d1534e1d555465f5b2f8dc0d2145e839b714
Harry Collins <harry@localhost.susx.ac.uk>
2018-03-16 12:17:56 +0000

    Harrys updated DB code 0.0.1

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
-
+const util = require('util');
 
 var MongoClient = require('mongodb').MongoClient;
-var uri = "mongodb://kay:UniNinja@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+//var uri = "mongodb://ESRSAdmin:UniNinja@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/UniNinjaDB?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+var uri = "mongodb://ESRSAdmin:UniNinja@cluster0-shard-00-00-d1bwx.mongodb.net:27017,cluster0-shard-00-01-d1bwx.mongodb.net:27017,cluster0-shard-00-02-d1bwx.mongodb.net:27017/uni?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+
 MongoClient.connect(uri, function(err, db) {
-  console.log('Connection to DB has been made!');
-  //db.close(); // remove this and it works
+  if (err) {
+    console.log("ERROR: " + err);
+  }
+   console.log("DB:" + db);
+   console.log(util.inspect(db));
+   console.log(db.collection("uni"));
 });
 
 
@@ -29,8 +35,19 @@ app.get('/', function (req, res) {
   } else if (req.query.username == "dan") {
     res.send('Android > iOS');
   } else {
-    res.send("The UniNinja API is coming soon!");
+    //res.send("The UniNinja API is coming soon!");
   }
+
+  if (req.query.uni === "all"){
+    MongoClient.connect(uri, function(err, db) {
+      console.log('Connection to DB has been made for uni list return!');
+      //db = db.getSiblingDB('uni');
+      //db.close(); // remove this and it work
+      //res.send(db.uni.find().pretty());
+      console.log('DATABASE: ' + db);
+    });
+  }
+
 });
 
 // run server on port 3000
