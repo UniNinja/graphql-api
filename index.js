e065be3f8fab43c3d2ef083ae95181a3605690db
Harry Collins <harry@Harrys-MacBook-Pro.local>
2018-03-23 12:29:31 +0000

    GraphQL API Working locally for MongoDB server & RoadMap added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import {schema} from './Schema'
+//import {schema} from './Schema'
+//import {resolver} from './resolvers'
 
-require('dotenv').config()
+//http://LMNL0GP6R3WGTPC4BPNC:password@data.unistats.ac.uk/api/v4/KIS/Institution/10007806/Courses.json?pageSize=300
+
+require('dotenv').config();
+import fetch from 'node-fetch';
 const util = require('util');
-
 
 // DATABASE SETUP
 var uri = process.env.MONGODB_URI;
@@ -11,7 +14,7 @@ var databaseErr = null;
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect(uri, function(err, connection) {
-  if(connection) {
+  if (connection) {
     database = connection.db(process.env.MONGODB_DATABASE);
   } else {
     databaseErr = err;
@@ -19,42 +22,48 @@ MongoClient.connect(uri, function(err, connection) {
 });
 
 // GRAPHQL SETUP
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const app = express()
 
+const {readFileSync} = require("fs");
+const bodyParser = require("body-parser");
+const {graphqlExpress, graphiqlExpress} = require("apollo-server-express");
+const {makeExecutableSchema} = require("graphql-tools");
 
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql');
+const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}))
-
-
-// CURRENT LISTENER
-app.get('/', function (req, res) {
-
-  // BASIC USERNAME LISTENER
-  if (req.query.username == "loic") {
-    res.send('iOS > Android');
-  } else if (req.query.username == "dan") {
-    res.send('Android > iOS');
-  } else {
-
-    // DATABASE QUERY
-    if(database) {
-      database.collection("uni").find().toArray(function(err, dbRes) {
-        if(err) console.log("Error");
-        console.log(dbRes);
-        res.send(dbRes);
-      });
-
-    // ELSE ERROR CONNECTING
-    } else {
-      res.send('Error connecting to database: '+databaseErr);
+const schema = makeExecutableSchema({
+  typeDefs: readFileSync("schema.graphql", "utf8"),
+  resolvers: {
+    Query: {
+      universities: () => getUniversities(),
+      courses: () => getCourses()
     }
   }
 });
+
+function getCourses() {
+  //console.log("Hi Dan");
+  const promise = fetch('http://unistats.ac.uk/api/v4/KIS/Institution/10007806/Courses.json', {
+    headers: {
+      'Authorization': 'Basic TE1OTDBHUDZSM1dHVFBDNEJQTkM6cGFzc3dvcmQK'
+    }
+  }
+).then(res => {
+  console.log(res);
+  return res.json();
+}).catch(err => console.log(err));
+
+  return promise;
+}
+
+function getUniversities() {
+  const promise = database.collection("uni").find().toArray().then(obj => obj).catch(err => console.log(err));
+  return promise;
+}
+
+app.use('/v0', graphqlHTTP({schema, graphiql: true}));
 
 // run server on port 3000
 app.listen('3000', _ => console.log('Server is listening on port 3000...'))
