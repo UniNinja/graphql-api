a68450bb138d93f9da1773994f1b699663757771
Harry Collins <harry@localhost.susx.ac.uk>
2018-04-13 19:27:25 +0100

    Working on getUniversities()

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ import fetch from 'node-fetch';
 const util = require('util');
 
 // DATABASE SETUP
-var uri = "mongodb://"
-            + process.env.MONGODB_USERNAME + ":"
-            + process.env.MONGODB_PASSWORD
-            + "@unininja-cluster-shard-00-00-d1bwx.mongodb.net:27017,"
-            + "unininja-cluster-shard-00-01-d1bwx.mongodb.net:27017,"
-            + "unininja-cluster-shard-00-02-d1bwx.mongodb.net:27017"
-            + "/uni?ssl=true&replicaSet=unininja-cluster-shard-0&authSource=admin";
+var uri = "mongodb://" + process.env.MONGODB_USERNAME + ":" + process.env.MONGODB_PASSWORD + "@unininja-cluster-shard-00-00-d1bwx.mongodb.net:27017," + "unininja-cluster-shard-00-01-d1bwx.mongodb.net:27017," + "unininja-cluster-shard-00-02-d1bwx.mongodb.net:27017" + "/uni?ssl=true&replicaSet=unininja-cluster-shard-0&authSource=admin";
 
 var database = null;
 var databaseErr = null;
@@ -50,19 +44,54 @@ function getCourses() {
     headers: {
       'Authorization': 'Basic TE1OTDBHUDZSM1dHVFBDNEJQTkM6cGFzc3dvcmQK'
     }
-  }
-).then(res => {
-  console.log(res);
-  return res.json();
-}).catch(err => console.log(err));
+  }).then(res => {
+    console.log(res);
+    return res.json();
+  }).catch(err => console.log(err));
 
   return promise;
 }
 
 function getUniversities() {
-  const promise = database.collection("uni").find().toArray().then(obj => obj).catch(err => console.log(err));
-  return promise;
+
+  fetch('https://data.unistats.ac.uk/api/v4/KIS/Institutions.json?pageSize=1000', {
+    headers: {
+      'Authorization': 'Basic TE1OTDBHUDZSM1dHVFBDNEJQTkM6cGFzc3dvcmQK'
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(myJson) {
+    var myUniList = myJson;
+    var expr = /university/;
+    for (var i = 0; i < myUniList.length; i++) {
+      //console.log("IN FOR LOOP: " + i);
+      if (!(myUniList[i].Name.toLowerCase().includes("university"))) {
+        console.log("ITEM DELETED: " + myUniList[i].Name.toLowerCase());
+        myUniList.splice(i, 1);
+      } else if(!(expr.test(myUniList[i].Name.toLowerCase()))){
+           console.log("ITEM DELETED 2: " + myUniList[i].Name.toLowerCase());
+           myUniList.splice(i, 1);
+      }
+
+      // IF WE NEED TO REPLACE THINGS ETC.
+      // }else {
+      //   var tempStr;
+      //   tempStr = JSON.stringify(myUniList[i]);
+      //   tempStr = tempStr.replace(/\"Name\":/g, "\"name\":");
+      //   myUniList[i] = JSON.parse(tempStr);
+      // }
+    }
+
+    console.log(myUniList);
+    console.log(myUniList.length);
+    return myUniList;
+  });
 }
+
+// function getUniversities() {
+//   const promise = database.collection("uni").find().toArray().then(obj => obj).catch(err => console.log(err));
+//   return promise;
+// }
 
 app.use('/v0', graphqlHTTP({schema, graphiql: true}));
 
