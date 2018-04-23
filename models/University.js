2acb6fb52f7f9a2e62de0a319af1c80eb5d52793
Harry Collins <harry@localhost.susx.ac.uk>
2018-04-23 15:48:27 +0100

    All code in Graphql-api is now ahearing to StandardJS

--- models/University.js
+++ models/University.js
@@ -4,14 +4,14 @@ class University {
    * @param  {String}  name      Text that describes the task to do
    * //@param  {Boolean} [done=false] True if the task is done
    */
-  constructor(name) {
-    this.id = ++University.counter;
-    this.name = name;
-    //this.done = done;
+  constructor (name) {
+    this.id = ++University.counter
+    this.name = name
+    // this.done = done;
   }
 }
 
 // counter of instances
-University.counter = 0;
+University.counter = 0
 
-module.exports = University;
+module.exports = University
