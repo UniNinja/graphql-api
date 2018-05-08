1a2820896a03aefe3e622b2106ded8922495edc3
Harry Collins <harry@localhost.susx.ac.uk>
2018-05-08 13:41:00 +0100

    Added test framework & corrected spelling mistake in schema

---
+++ tests/index.test.js
@@ -0,0 +1,30 @@
+test('Demo Test', () => {
+  expect(0).toBe(0)
+})
+
+describe('A user', function() {
+  it('should register with new user', () => {
+    expect(true).toBe(true);
+  });
+  it('should not register with existing user data', () => {
+    expect(true).toBe(true);
+  });
+  it('should not login with wrong credentials', () => {
+    expect(true).toBe(true);
+  });
+  it('should login with correct credentials', () => {
+    expect(true).toBe(true);
+  });
+  it('should not login twice', () => {
+    expect(true).toBe(true);
+  });
+  it('should logout after logging in', () => {
+    expect(true).toBe(true);
+  });
+  it('should not logout if not logged in', () => {
+    expect(true).toBe(true);
+  });
+  it('should removed by ID', () => {
+    expect(true).toBe(true);
+  });
+});
